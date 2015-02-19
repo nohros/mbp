de74719e788eb20169a59507efa3f9d27cffcf30
nohros <nohros.ceo@gmail.com>
2015-02-19 20:50:11 -0300

    Add index:build task

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,35 +3,48 @@
 // Load required Grunt tasks. These are installed based on the versions
 // listed in "packages.json" when you do "npm install" in this directory.
 
+var fs = require("fs");
+var path = require("path");
+var del = require("del");
+
 var gulp = require("gulp");
 var gutil = require("gulp-util");
-var merge = require('gulp-if');
-var del = require("del");
+var gulpif = require('gulp-if');
 var copy = require("gulp-copy");
-var template = require("gulp-template");
 var concat = require("gulp-concat");
-//var jshint = require("gulp-jshint");
 var uglify = require("gulp-uglify");
-//var watch = require("gulp-watch");
 var bump = require("gulp-bump");
-var fs = require("fs");
+var moment = require("moment");
+var header = require("gulp-header");
+var tap = require("gulp-tap");
+
+//var jshint = require("gulp-jshint");
+//var watch = require("gulp-watch");
   
 // load in out build configuration file.
 var cfg = require('./build.config.js');
 
+// We are reading the file through fs because teh bump task needs to
+// overwrite it and we cannot do it if the file is readed using the
+// require() function.
 var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
-var banner =
-    '/**\n' +
-    ' * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd" %>) %>\n' +
-    ' * <%= pkg.homepage %>\n' +
-    ' * \n' +
-    ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-    ' * Licensed <%= pkg.license.type %>\n' +
-    ' */\n';
+var today = moment();
 
-var asset_suffix = cfg.assets_dir + '/' + pkg.name + '-' + pkg.version;
-    
+var banner =
+  '/**\n' +
+  ' * <%= pkg.name %> - v<%= pkg.version %> - <%= today.format("yyyy-mm-dd") %>)\n' +
+  ' * <%= pkg.homepage %>\n' +
+  ' * \n' +
+  ' * Copyright (c) <%= today.format("yyyy") %> <%= pkg.author %>\n' +
+  ' * Licensed <%= pkg.license %>\n' +
+  ' */\n';
+
+var asset_suffix = path.join(cfg.assets_dir, pkg.name + '-' + pkg.version);
+
+// we'll keep the name of script files and styles in memory.
+var scripts = {}, styles = {};
+
 /**
  * Increments the version number, etc.
  */
@@ -44,8 +57,8 @@ gulp.task('bump', function () {
 /**
  * The directories to delete when `gulp clean` is executed.
  */
-gulp.task('clean', function (cb) {
-  del([cfg.build_dir, cfg.compile_dir], cb);
+gulp.task('clean', function () {
+  del([cfg.build_dir, cfg.compile_dir]);
 });
 
 /**
@@ -54,13 +67,13 @@ gulp.task('clean', function (cb) {
  * `build-dir`, and then to copy the assets to `compile_dir`.
  */
 gulp.task('copy:build_app_assets', function () {
-  gulp.src('./src/' + cfg.assets_dir + '/**')
-    .pipe(copy(cfg.build_dir + '/' + cfg.assets_dir));
+  gulp.src(path.join('.', 'src', cfg.assets_dir, '**'))
+    .pipe(copy(path.join(cfg.build_dir, cfg.assets_dir)));
 });
 
 gulp.task('copy:build_vendor_assets', function () {
   gulp.src(cfg.vendor_files.assets)
-    .pipe(copy(cfg.build_dir + '/' + cfg.assets_dir));
+    .pipe(copy(path.join(cfg.build_dir, cfg.assets_dir)));
 });
 
 gulp.task('copy:build_appjs', function () {
@@ -79,7 +92,7 @@ gulp.task('copy:build_vendorcss', function () {
 });
 
 gulp.task('copy:compile_assets', function () {
-  gulp.src(cfg.build_dir + '/' + cfg.assets_dir + '/**')
+  gulp.src(path.join(cfg.build_dir, cfg.assets_dir, '**'))
     .pipe(copy(cfg.compile_dir + cfg.assets_dir));
   
   gulp.src(cfg.vendor_files.css)
@@ -92,37 +105,55 @@ gulp.task('copy:compile_assets', function () {
  */
 gulp.task('concat:css', function () {
   gulp.src(cfg.vendor_files.css)
-    .pipe(concat(cfg.build_dir + '/' + asset_suffix + '.css'));
+    .pipe(concat(path.join(cfg.build_dir, asset_suffix + '.css')));
 });
 
 /**
  * Concatenates multiple js files into a single js file and minifies it
  * we are compiling.
  */
-gulp.task('concat:js', function (build) {
+gulp.task('concat:js', function () {
   var src = cfg.vendor_files.js.concat([
-    './module.prefix',
-    cfg.build_dir + '/src/**/*.js',
-    './module.suffix'
+    'module.prefix',
+    path.join(cfg.build_dir, 'src', '**', '*.js'),
+    'module.suffix'
   ]);
   
   gulp.src(src)
-    .gulpif(!build, uglify())
-    .pipe(gulp.dest(cfg.compile_dir + '/' + asset_suffix + '.js'));
+    .pipe(header(banner, {pkg : pkg}))
+    .pipe(gulp.dest(path.join(cfg.compile_dir, asset_suffix + '.js')));
+});
+
+/**
+ * Minify the compiled sources.
+ */
+gulp.task('uglify', function () {
+  gulp.src(path.join(cfg.compile_dir, asset_suffix + '.js'))
+    .pipe(uglify());
 });
 
 /**
  * This task compiles the index.html file as a Gulp Lo-Dash/Underscore
- * template. CSS and JS files co-exist here but they get split apart later.
+ * template. During development, we don't want to have to wait for
+ * compilation, concatenation, minification, etc. So to avoid this steps,
+ * we simply add all scripts files directly to the `<head>` of `index.html`.
  */
-gulp.task('index', function (build) {
-  var src;
-  if (!build) {
-    src = cfg.vendor_files.js.concat([
-      cfg.vendor_files.js,
-      cfg.build_dir + '/src/**/*.js',
-      cfg.vendor_files.css,
-      cfg.build_dir + asset_suffix + '.css'
-    ]);
-  }
+gulp.task('index:build', function () {
+  var files = cfg.vendor_files.js.concat([
+    path.join(cfg.build_dir, 'src', '**', '*.js'),
+    path.join(cfg.build_dir, 'assets', asset_suffix + '.css')
+  ]).concat(cfg.vendor_files.css);
+
+  return gulp.src(files)
+    .pipe(tap(function (file) {
+      if (path.extname(file.path) === '.js') {
+        scripts[path.basename(file.path)] = 0;
+      } else {
+        styles[path.basename(file.path)] = 0;
+      }
+    }));
+});
+
+gulp.task('default', ['clean', 'index:build'], function () {
+  console.log(scripts);
 });
